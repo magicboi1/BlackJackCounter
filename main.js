b548384b4e232e82f9187d8adaa830f4ea7d052a
magicboi1 <95828708+magicboi1@users.noreply.github.com>
2022-06-11 15:58:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-let add = document.getElementById("increment");
-let remove = document.getElementById("decrement");
-
+let add = document.getElementById("right");
+let remove = document.getElementById("left");
+let reset = document.getElementById("reset");
 let int = document.getElementById("number");
 let integer = 0;
+let img = document.getElementById("showImage");
+
+window.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    integer += 1;
+    int.innerHTML = integer;
+  }
+
+  if (e.key === "ArrowLeft") {
+    integer -= 1;
+    int.innerHTML = integer;
+  }
+
+  if (e.key === "r") {
+    integer = 0;
+    int.innerHTML = integer;
+  }
+});
 
 add.addEventListener("click", function () {
   integer += 1;
@@ -13,3 +31,16 @@ remove.addEventListener("click", function () {
   integer -= 1;
   int.innerHTML = integer;
 });
+
+reset.addEventListener("click", function () {
+  integer = 0;
+  int.innerHTML = integer;
+});
+
+document.getElementById("showImage").onclick = function () {
+  if (document.getElementById("theImage").style.visibility == "hidden") {
+    document.getElementById("theImage").style.visibility = "visible";
+  } else {
+    document.getElementById("theImage").style.visibility = "hidden";
+  }
+};
